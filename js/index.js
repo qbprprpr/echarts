0c19aadd74860739329a24b6ca1b44902bcb72ee
qbprprpr <243782580@qq.com>
2021-03-20 16:35:47 +0800

    echarts finish

--- js/index.js
+++ js/index.js
@@ -598,3 +598,67 @@ var yearData = [
 		myChart.resize()
 	})
 })();
+
+// 饼状图2
+(function () {
+	let myChart = echarts.init(document.querySelector(".pie2 .chart"))
+	let option = {
+		color: ['#006cff', '#60cda0', '#ed8884', '#ff9f7f', '#0096ff', '#9fe6b8', '#32c5e9', '#1d9dff'],
+		legend: {
+			bottom: '0%',
+			itemWidth: 10,
+			itemHeight: 10,
+			textStyle: {
+				color: 'rgba(255, 255, 255, .5)',
+				fontSize: 12
+			}
+		},
+		toolbox: {
+			show: true,
+			feature: {
+				mark: { show: true },
+				dataView: { show: false, readOnly: false },
+				restore: { show: false },
+				saveAsImage: { show: false }
+			}
+		},
+		series: [
+			{
+				name: '地区分布',
+				type: 'pie',
+				radius: ["10%", "70%"],
+				center: ['50%', '50%'],
+				roseType: 'radius',
+				// 图形文字标签
+				lavel: {
+					fontSize: 10
+				},
+				// 连接饼图和文字的线条
+				labelLine: {
+					// 连接图形的线条
+					length: 6,
+					// 连接文字的线条
+					length2: 8
+				},
+				itemStyle: {
+					borderRadius: 8
+				},
+				data: [
+					{ value: 20, name: "云南" },
+          { value: 26, name: "北京" },
+          { value: 24, name: "山东" },
+          { value: 25, name: "河北" },
+          { value: 20, name: "江苏" },
+          { value: 25, name: "浙江" },
+          { value: 30, name: "深圳" },
+          { value: 42, name: "广东" }
+				]
+			}
+		]
+	}
+	myChart.setOption(option);
+	// 让图表跟随屏幕自动适应
+	window.addEventListener("resize", function () {
+		myChart.resize()
+	})
+})();
